ba127dadc942b144ca5f51c65dfeff00d02cc948
Mehdi Taherabadi <MehdiTaherabadi@outlook.com>
2020-09-14 17:00:42 +0430

    Add Context And Create Domin Layer

---
+++ TopDev/CleanArch.Domin/Modals/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArch.Domin.Modals
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string Descpertios { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ TopDev/CleanArch.Infra.Data/Context/TopDevContext.cs
@@ -0,0 +1,15 @@
+﻿using CleanArch.Domin.Modals;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class TopDevContext : DbContext
+    {
+        public TopDevContext(DbContextOptions<TopDevContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
