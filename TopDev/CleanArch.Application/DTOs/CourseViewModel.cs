23d95effb58f4c03e914d8eb96377c8b68749dcf
Mehdi Taherabadi <MehdiTaherabadi@outlook.com>
2020-09-15 13:27:35 +0430

    Add Repositroy And Service

---
+++ TopDev/CleanArch.Application/DTOs/CourseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CleanArch.Domin.Modals;
+using System.Collections.Generic;
+
+namespace CleanArch.Application.DTOs
+{
+    public class CourseViewModel
+    {
+        public List<Course> Courses { get; set; }
+    }
+}
---
+++ TopDev/CleanArch.Domin/Interfaces/ICourseRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanArch.Domin.Interfaces
+{
+    public class ICourseRepository
+    {
+        
+    }
+}
